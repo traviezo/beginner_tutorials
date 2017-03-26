781a28b600789fcd50764c59b6dc845f256a5edf
Traviezo <perseverancia@hotmail.com>
2017-03-26 03:52:52 -0400

    Modified talker.cpp to correct cpplint errors

--- src/talker.cpp
+++ src/talker.cpp
@@ -1,13 +1,13 @@
-#include "ros/ros.h"
-#include "std_msgs/String.h"
+/* Copyright 2017 Christian */
 
 #include <sstream>
+#include "ros/ros.h"
+#include "std_msgs/String.h"
 
 /**
  * This tutorial demonstrates simple sending of messages over the ROS system.
  */
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv) {
   /**
    * The ros::init() function needs to see argc and argv so that it can perform
    * any ROS arguments and name remapping that were provided at the command line.
@@ -53,8 +53,7 @@ int main(int argc, char **argv)
    * a unique string for each message.
    */
   int year = 2017;
-  while (ros::ok())
-  {
+  while (ros::ok()) {
     /**
      * This is a message object. You stuff it with data, and then publish it.
      */
